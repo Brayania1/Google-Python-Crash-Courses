00e60feed290a930e8a77ead62c011a913de42cb
Cassie Hall <casshall@cisco.com>
2023-03-02 14:46:05 -0500

    Test

---
+++ arthmetic_operators.py
@@ -0,0 +1,12 @@
+#Multiplication, division, addition, and subtraction
+print(3*8/2+5-1)
+
+#Exponents
+print(4**6) #Sytax means 4 to the power of 6
+print("4**2") # To square a number
+print(4**3) # To cube a number
+print(4**0.5) # To find the square root fo a number
+
+# To calculate how many different possible combination can be 
+# formed using a set of "x" characters with each charater in "x"
+# having "y"number of possible values, you will need to use an exponent for the calculation:
